dd22dea2d59b64ec4d7e831963a83c11a592763e
Oscar <oskr1087lema@gmail.com>
2026-07-10 14:37:13 -0500

    [FIX] Date order

--- jkk_report/models/purchase_order.py
+++ jkk_report/models/purchase_order.py
@@ -11,7 +11,6 @@ class PurchaseOrder(models.Model):
         compute="_compute_estimated_payment_date",
     )
 
-    @api.depends("date_order", "partner_id")
     def _compute_estimated_payment_date(self):
         for order in self:
             order.estimated_payment_date = False
@@ -19,9 +18,7 @@ class PurchaseOrder(models.Model):
             payment_term = order.partner_id.property_supplier_payment_term_id
             if not payment_term or not order.date_order:
                 continue
-
-            days = max(payment_term.line_ids.mapped("days") or [0])
-
+            days = max(payment_term.line_ids.mapped("nb_days") or [0])
             order.estimated_payment_date = fields.Date.to_date(
                 order.date_order
             ) + relativedelta(days=days)
